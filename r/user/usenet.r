73b5efb6ca2646052dee40fc772fc81b785673ee
JBris <james.a.bristow.contact@gmail.com>
2020-04-08 03:01:33 +1200

    Adding N-gram analysis to usenet script

--- r/user/usenet.r
+++ r/user/usenet.r
@@ -6,6 +6,8 @@ library(dplyr)
 library(tidyr)
 library(purrr)
 library(readr)
+library(stringr)
+library(tidytext)
 
 training_folder <- "~/data/20news-bydate/20news-bydate-train/"
 
@@ -25,3 +27,130 @@ raw_text <- tibble(folder = dir(training_folder, full.names = TRUE)) %>%
   transmute(newsgroup = basename(folder), id, text)
 
 raw_text
+
+library(ggplot2)
+
+raw_text %>%
+  group_by(newsgroup) %>%
+  summarize(messages = n_distinct(id)) %>%
+  ggplot(aes(newsgroup, messages)) +
+  geom_col() +
+  coord_flip()
+
+# Clean and preprocess data
+
+# must occur after the first occurrence of an empty line,
+# and before the first occurrence of a line starting with --
+cleaned_text <- raw_text %>%
+  group_by(newsgroup, id) %>%
+  filter(cumsum(text == "") > 0,
+         cumsum(str_detect(text, "^--")) == 0) %>%
+  ungroup()
+
+cleaned_text <- cleaned_text %>%
+  filter(str_detect(text, "^[^>]+[A-Za-z\\d]") | text == "",
+         !str_detect(text, "writes(:|\\.\\.\\.)$"),
+         !str_detect(text, "^In article <"),
+         !id %in% c(9704, 9985))
+
+usenet_words <- cleaned_text %>%
+  unnest_tokens(word, text) %>%
+  filter(str_detect(word, "[a-z']$"),
+         !word %in% stop_words$word)
+
+words_by_newsgroup <- usenet_words %>%
+  count(newsgroup, word, sort = TRUE) %>%
+  ungroup()
+
+words_by_newsgroup
+
+# Perform sentiment analysis
+
+newsgroup_sentiments <- words_by_newsgroup %>%
+  inner_join(get_sentiments("afinn"), by = "word") %>%
+  group_by(newsgroup) %>%
+  summarize(value = sum(value * n) / sum(n))
+
+newsgroup_sentiments %>%
+  mutate(newsgroup = reorder(newsgroup, value)) %>%
+  ggplot(aes(newsgroup, value, fill = value > 0)) +
+  geom_col(show.legend = FALSE) +
+  coord_flip() +
+  ylab("Average sentiment value")
+
+# Sentiment analysis by word
+contributions <- usenet_words %>%
+  inner_join(get_sentiments("afinn"), by = "word") %>%
+  group_by(word) %>%
+  summarize(occurences = n(),
+            contribution = sum(value))
+
+contributions
+
+contributions %>%
+  top_n(25, abs(contribution)) %>%
+  mutate(word = reorder(word, contribution)) %>%
+  ggplot(aes(word, contribution, fill = contribution > 0)) +
+  geom_col(show.legend = FALSE) +
+  coord_flip()
+
+top_sentiment_words <- words_by_newsgroup %>%
+  inner_join(get_sentiments("afinn"), by = "word") %>%
+  mutate(contribution = value * n / sum(n))
+
+top_sentiment_words
+
+# Sentiment by message
+sentiment_messages <- usenet_words %>%
+  inner_join(get_sentiments("afinn"), by = "word") %>%
+  group_by(newsgroup, id) %>%
+  summarize(sentiment = mean(value),
+            words = n()) %>%
+  ungroup() %>%
+  filter(words >= 5)
+
+sentiment_messages %>%
+  arrange(desc(sentiment))
+
+print_message <- function(group, message_id) {
+  result <- cleaned_text %>%
+    filter(newsgroup == group, id == message_id, text != "")
+  
+  cat(result$text, sep = "\n")
+}
+
+print_message("rec.sport.hockey", 53560)
+
+sentiment_messages %>%
+  arrange(sentiment)
+
+print_message("rec.sport.hockey", 53907)
+
+# N-gram analysis
+usenet_bigrams <- cleaned_text %>%
+  unnest_tokens(bigram, text, token = "ngrams", n = 2)
+
+usenet_bigram_counts <- usenet_bigrams %>%
+  count(newsgroup, bigram, sort = TRUE) %>%
+  separate(bigram, c("word1", "word2"), sep = " ")
+
+negate_words <- c("not", "without", "no", "can't", "don't", "won't")
+
+usenet_bigram_counts %>%
+  filter(word1 %in% negate_words) %>%
+  count(word1, word2, wt = n, sort = TRUE) %>%
+  inner_join(get_sentiments("afinn"), by = c(word2 = "word")) %>%
+  mutate(contribution = value * n) %>%
+  group_by(word1) %>%
+  top_n(10, abs(contribution)) %>%
+  ungroup() %>%
+  mutate(word2 = reorder(paste(word2, word1, sep = "__"), contribution)) %>%
+  ggplot(aes(word2, contribution, fill = contribution > 0)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ word1, scales = "free", nrow = 3) +
+  scale_x_discrete(labels = function(x) gsub("__.+$", "", x)) +
+  xlab("Words preceded by a negation") +
+  ylab("Sentiment value * # of occurrences") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  coord_flip()
+  
